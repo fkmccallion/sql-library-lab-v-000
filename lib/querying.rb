3be5393f85f0545610569169de7ed2304c20b28e
Frank McCallion <fkmccallion@gmail.com>
2018-03-23 00:25:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors"
+  "SELECT authors.name, subgenres.name FROM series JOIN authors ON authors.id = series.author_ID JOIN subgenres ON subgenres.id = subgenre_ID"
 end
 
 def select_series_title_with_most_human_characters

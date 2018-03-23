c641c64b34f74d4f7f72ee4178cd8f386e88a47d
Frank McCallion <fkmccallion@gmail.com>
2018-03-23 00:21:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT * FROM authors"
+  "SELECT author.name, subgenres.name FROM authors"
 end
 
 def select_series_title_with_most_human_characters
